f88ccfaea00688426350e866b328b2d4f6c1d3a2
TheMightyShiv <shiv@areax-sky.net>
2013-09-17 23:51:41 -0400

    Released.: Version 1.4.1 Modified.: Cosmetics and Code Cleanup

--- BuildVPN.sh
+++ BuildVPN.sh
@@ -18,12 +18,9 @@ func_title(){
   clear
 
   # Print Title
-  echo '============================================================================'
-  echo ' BuildVPN.sh | [Version]: 1.4.0 | [Updated]: 08.28.2013'
-  echo '============================================================================'
-  echo ' [By]: Michael Wright | [GitHub]: https://github.com/themightyshiv'
-  echo '============================================================================'
-  echo
+  echo '================================================================'
+  echo ' BuildVPN.sh | [Version]: 1.4.1 | [Updated]: 09.17.2013'
+  echo '================================================================'
 }
 
 # Server Install Function
@@ -41,55 +38,57 @@ func_build_server(){
   echo ' 1 = Debian (5/6/7)'
   echo ' 2 = Ubuntu (12.04)'
   echo
-  read -p 'Enter Operating System..........................: ' os
+  read -p 'Enter Operating System.....................: ' os
   # Retry For People Who Don't Read Well
-  if [ "${os}" != '1' ] && [ "${os}" != '2' ]
+  if [ ${os} != '1' ] && [ ${os} != '2' ]
   then
     clear
     func_title
+    echo
     func_build_server
   fi
-  read -p 'Enter Server Hostname...........................: ' host
+  read -p 'Enter Server Hostname......................: ' host
   echo
   echo '+----------------------+'
   echo '| Available Interfaces |'
   echo '+----------------------+'
   ifconfig |awk "/Link|inet/"|tr -s '[:space:]'|sed 's/ Link.*//g'|sed -e ':a;N;$!ba;s/\n inet//g' -e 's/addr://g'|cut -d" " -f 1,2|sed 's/ /\t/g'
   echo
-  read -p 'Enter IP OpenVPN Server Will Bind To............: ' ip
-  read -p 'Enter Subnet For VPN (ex: 192.168.100.0)........: ' vpnnet
-  read -p 'Enter Subnet Netmask (ex: 255.255.255.0)........: ' netmsk
-  read -p 'Enter Preferred DNS Server (ex: 208.67.222.222).: ' dns
-  read -p 'Enter Max Clients Threshold.....................: ' maxconn
-  read -p 'Increase Encryption Key Size To 2048 (y/n)......: ' incbits
-  read -p 'Route All Traffic Through This VPN (y/n)........: ' routeall
-  read -p 'Allow Certificates With Same Subject (y/n)......: ' unique
-  read -p 'Enable IP Forwarding (y/n)......................: ' forward
+  read -p 'Enter IP OpenVPN Server Will Bind To.......: ' ip
+  read -p 'Enter Subnet For VPN (ex: 192.168.100.0)...: ' vpnnet
+  read -p 'Enter Subnet Netmask (ex: 255.255.255.0)...: ' netmsk
+  read -p 'Enter Preferred DNS Server (ex: 8.8.8.8)...: ' dns
+  read -p 'Enter Max Clients Threshold................: ' maxconn
+  read -p 'Increase Encryption Key Size To 2048 (y/n).: ' incbits
+  read -p 'Route All Traffic Through This VPN (y/n)...: ' routeall
+  read -p 'Allow Certificates With Same Subject (y/n).: ' unique
+  read -p 'Enable IP Forwarding (y/n).................: ' forward
   # Determine What IP Protocols To Forward For
-  if [[ "${forward}" == [yY] ]]
+  if [[ ${forward} == [yY] ]]
   then
-    read -p 'Forward IPv4 (y/n)..............................: ' forward4
-    read -p 'Forward IPv6 (y/n)..............................: ' forward6
+    read -p 'Forward IPv4 (y/n).........................: ' forward4
+    read -p 'Forward IPv6 (y/n).........................: ' forward6
   fi
-  read -p 'Enable IPTables Masquerading (y/n)..............: ' enablenat
+  read -p 'Enable IPTables Masquerading (y/n).........: ' enablenat
   # Determine What Interface To Use For Masquerading
-  if [[ "${enablenat}" == [yY] ]]
+  if [[ ${enablenat} == [yY] ]]
   then
-    read -p 'Enter Interface To Use For NAT..................: ' natif
+    read -p 'Enter Interface To Use For NAT.............: ' natif
   fi
 
   # Build Certificate Authority
   func_title
+  echo
   echo '[*] Preparing Directories'
   cp -R ${easyrsa_tmp} ${easyrsa_dir}
   cd ${easyrsa_dir}
   # Modify OpenSSL Variables For 2048 Bit Encryption
-  if [[ "${incbits}" == [yY] ]]
+  if [[ ${incbits} == [yY] ]]
   then
     sed -i 's/KEY_SIZE=1024/KEY_SIZE=2048/g' vars
   fi
   # Workaround For Ubuntu 12.x
-  if [ "${os}" == '2' ]
+  if [ ${os} == '2' ]
   then
     echo '[*] Preparing Ubuntu Config File'
     cp openssl-1.0.0.cnf openssl.cnf
@@ -101,12 +100,15 @@ func_build_server(){
   echo '[*] Building Certificate Authority'
   ./build-ca
   func_title
+  echo
   echo '[*] Building Key Server'
   ./build-key-server ${host}
   func_title
+  echo
   echo '[*] Generating Diffie Hellman Key'
   ./build-dh
   func_title
+  echo
   cd ${ovpnkey_dir}
   echo '[*] Generating TLS-Auth Key'
   openvpn --genkey --secret ta.key
@@ -121,7 +123,7 @@ func_build_server(){
   echo "cert ${ovpnkey_dir}/${host}.crt" >> ${ovpnsvr_cnf}
   echo "key ${ovpnkey_dir}/${host}.key" >> ${ovpnsvr_cnf}
   # Determine If Increased Key Size Option Was Chosen
-  if [[ "${incbits}" == [yY] ]]
+  if [[ ${incbits} == [yY] ]]
   then
     echo "dh ${ovpnkey_dir}/dh2048.pem" >> ${ovpnsvr_cnf}
   else
@@ -130,12 +132,12 @@ func_build_server(){
   echo "server ${vpnnet} ${netmsk}" >> ${ovpnsvr_cnf}
   echo 'ifconfig-pool-persist ipp.txt' >> ${ovpnsvr_cnf}
   # Determine If Route All Traffic Option Was Chosen
-  if [[ "${routeall}" == [yY] ]]
+  if [[ ${routeall} == [yY] ]]
   then
     echo 'push "redirect-gateway def1"' >> ${ovpnsvr_cnf}
   fi
   # Determine If Unique Subjects Option Was Chosen
-  if [[ "${unique}" == [yY] ]]
+  if [[ ${unique} == [yY] ]]
   then
     echo 'unique_subject = no' >> ${openvpn_dir}/easy-rsa/keys/index.txt.attr
   fi
@@ -154,7 +156,7 @@ func_build_server(){
   echo 'mute 20' >> ${ovpnsvr_cnf}
 
   # Determine If IPv4 Forwarding Option Was Chosen
-  if [[ "${forward4}" == [yY] ]]
+  if [[ ${forward4} == [yY] ]]
   then
     echo '[*] Enabling IPv4 Forwarding In /etc/sysctl.conf'
     sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
@@ -163,7 +165,7 @@ func_build_server(){
   fi
 
   # Determine If IPv6 Forwarding Option Was Chosen
-  if [[ "${forward6}" == [yY] ]]
+  if [[ ${forward6} == [yY] ]]
   then
     echo '[*] Enabling IPv6 Forwarding In /etc/sysctl.conf'
     sed -i 's/#net.ipv6.conf.all.forwarding=1/net.ipv6.conf.all.forwarding=1/' /etc/sysctl.conf
@@ -172,7 +174,7 @@ func_build_server(){
   fi
 
   # Determine If IPTables NAT Option Was Chosen
-  if [[ "${enablenat}" == [yY] ]]
+  if [[ ${enablenat} == [yY] ]]
   then
     echo '[*] Loading IPTables Masquerading Rule Into Current Ruleset'
     /sbin/iptables -t nat -A POSTROUTING -o ${natif} -j MASQUERADE
@@ -190,24 +192,25 @@ func_build_server(){
 # Build Client Certificates Function
 func_build_client(){
   # Get User Input
-  read -p 'Enter Username (No Spaces)......................: ' user
-  read -p 'Enter Name For Configuration File (No Spaces)...: ' confname
+  read -p 'Enter Username (No Spaces).....................: ' user
+  read -p 'Enter Name For Configuration File (No Spaces)..: ' confname
   echo
   echo '+------------------------+'
   echo '| Available IP Addresses |'
   echo '+------------------------+'
   ifconfig |awk "/Link|inet/"|tr -s '[:space:]'|sed 's/ Link.*//g'|sed -e ':a;N;$!ba;s/\n inet//g' -e 's/addr://g'|cut -d" " -f 1,2|sed 's/ /\t/g'
   echo
-  read -p 'Enter IP/Hostname OpenVPN Server Binds To.......: ' ip
-  read -p 'Will This Client Run Under Windows (y/n)........: ' windows
+  read -p 'Enter IP/Hostname OpenVPN Server Binds To......: ' ip
+  read -p 'Will This Client Run Under Windows (y/n).......: ' windows
   # Additional Configuration For Windows Clients
-  if [[ "${windows}" == [yY] ]]
+  if [[ ${windows} == [yY] ]]
   then
-    read -p 'Enter Node Name (Required For Windows Clients)..: ' node
+    read -p 'Enter Node Name (Required For Windows Clients).: ' node
   fi
   
   # Build Certificate
   func_title
+  echo
   echo "[*] Generating Client Certificate For: ${user}"
   cd ${easyrsa_dir}
   . ./vars
@@ -218,11 +221,12 @@ func_build_client(){
   
   # Build Client Configuration
   func_title
+  echo
   echo '[*] Creating Client Configuration'
   echo 'client' > ${user}/${confname}.ovpn
   echo 'dev tun' >> ${user}/${confname}.ovpn
   # Determine If Windows Options Were Chosen
-  if [[ "${windows}" == [yY] ]]
+  if [[ ${windows} == [yY] ]]
   then
     echo "dev-node ${node}" >> ${user}/${confname}.ovpn
   fi
@@ -231,7 +235,7 @@ func_build_client(){
   echo 'resolv-retry infinite' >> ${user}/${confname}.ovpn
   echo 'nobind' >> ${user}/${confname}.ovpn
   # Set Unprivileged User And Group For Linux Clients
-  if [[ "${windows}" != [yY] ]]
+  if [[ ${windows} != [yY] ]]
   then
     echo 'user nobody' >> ${user}/${confname}.ovpn
     echo 'group nogroup' >> ${user}/${confname}.ovpn
@@ -289,6 +293,7 @@ func_update(){
 if [ `whoami` != 'root' ]
 then
   func_title
+  echo
   echo '[ERROR]: You must run this script with root privileges.'
   echo
   exit 1
@@ -297,24 +302,31 @@ fi
 # Select Function and Menu Statement
 func_title
 case ${1} in
-  -i|--install)
+  -i)
+    echo
     func_install
     ;;
-  -s|--server)
+  -s)
+    echo
     func_build_server
     ;;
-  -c|--client)
+  -c)
+    echo
     func_build_client
     ;;
-  -u|--update)
+  -u)
+    echo
     func_update
     ;;
   *)
-    echo ' Usage...: ./BuildVPN.sh [OPTION]'
-    echo ' Options.:'
-    echo '           -i | --install = Install OpenVPN Packages'
-    echo '           -s | --server  = Build Server Configuration'
-    echo '           -c | --client  = Build Client Configuration'
-    echo '           -u | --update  = Update BuildVPN Script'
+    echo ' [By]: Mike Wright | [GitHub]: http://github.com/themightyshiv'
+    echo '================================================================'
+    echo
+    echo "[Usage]...: ${0} [OPTION]"
+    echo '[Options].:'
+    echo '            -i = Install OpenVPN Packages'
+    echo '            -s = Build Server Configuration'
+    echo '            -c = Build Client Configuration'
+    echo '            -u = Update BuildVPN Script'
     echo
 esac
