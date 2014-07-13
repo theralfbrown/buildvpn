2b95c24f78e1216f76548d60149a148cda65eb1d
themightyshiv <shiv@areax.io>
2014-07-13 04:32:27 -0400

    Released.: Version 1.6.0 Added....: OS Auto-Detection Added....: easy-rsa Auto-Detection (To Remove Workarounds) Fixed....: Interfaces Not Showing Properly on RHEL-Based OS Modified.: Cosmetics Removed..: IPv6 Support (Broken, Only Temporary) Removed..: dev-node Configuration

--- BuildVPN.sh
+++ BuildVPN.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Global Variables
+easyrsa_fnd=$(find / -wholename "*/easy-rsa/2.0")
 openvpn_dir='/etc/openvpn'
 easyrsa_dir='/etc/openvpn/easy-rsa'
 ovpnkey_dir='/etc/openvpn/easy-rsa/keys'
@@ -13,39 +14,58 @@ func_title(){
 
   # Print Title
   echo '================================================================'
-  echo ' BuildVPN.sh | [Version]: 1.5.1 | [Updated]: 03.06.2014'
+  echo ' BuildVPN.sh | [Version]: 1.6.0 | [Updated]: 07.13.2014'
   echo '================================================================'
 }
 
 # Supported OS Function
 func_os(){
   # Print Supported OS
+  echo
   echo '[ Supported Operating Systems ]'
   echo
-  echo ' 1 = Debian......(5+)'
-  echo ' 2 = Ubuntu......(12+)'
-  echo ' 3 = RHEL/CentOS.(6+)'
+  echo ' 1 = Debian-Based.(Versions 5+)'
+  echo ' 2 = RHEL-Based...(Versions 5+)'
   echo
 }
 
 # Server Install Function
 func_install(){
-  # Get User Input
-  func_os
-  read -p 'Enter Operating System.....................: ' os
+  # Attempt OS Auto-Detection
+  if [ -f /etc/redhat-release ]; then
+    host_os='RHEL'
+    epel_ver=$(cat /etc/redhat-release|cut -d" " -f4|cut -d"." -f1)
+  else
+    host_os=$(cat /etc/issue.net|cut -d" " -f1)
+  fi
+  # Determine OS
+  if [[ ${host_os} == 'Debian' ]]; then
+    os='1'
+  elif [[ ${host_os} == 'RHEL' ]]; then
+    os='2'
+  else
+    echo '[!] Warning: Attempting install on unsupported OS.'
+    echo '[!] Installation may still be successful.'
+    echo '[!] Please provide the following information.'
+    func_os
+    read -p 'Enter Operating System.....................: ' os
+  fi
   # OpenVPN Installer Statement
-  if [[ ${os} =~ [1-2] ]]; then
+  if [ ${os} == '1' ]; then
     # Install Using Apt-Get (Debian-Based)
     func_title
     echo
+    echo '[*] Operating System: Debian-Based'
     echo '[*] Updating Apt Sources'
     apt-get update
     echo
     echo '[*] Installing Packages'
     apt-get -y install openvpn openssl
-  elif [ ${os} == '3' ]; then
+  elif [ ${os} == '2' ]; then
     # Install Using Yum (RHEL-Based)
     func_title
+    echo
+    echo '[*] Operating System: RHEL-Based'
     echo
     echo '[ EPEL Repository Required ]'
     echo
@@ -56,7 +76,23 @@ func_install(){
       func_title
       echo
       echo '[*] Installing EPEL Repository'
-      rpm -ivh ftp://mirror.cs.princeton.edu/pub/mirrors/fedora-epel/6/i386/epel-release-6-8.noarch.rpm
+      case ${epel_ver} in
+        5)
+          rpm -ivh http://mirrors.kernel.org/fedora-epel/5/i386/epel-release-5-4.noarch.rpm
+          ;;
+        6)
+          rpm -ivh http://fedora-epel.mirror.lstn.net/6/i386/epel-release-6-8.noarch.rpm
+          ;;
+        7)
+          rpm -ivh http://ftp.cse.buffalo.edu/pub/epel/beta/7/x86_64/epel-release-7-0.2.noarch.rpm
+          ;;
+        *)
+          func_title
+          echo
+          echo '[!] Error: Unsupported RHEL-Based OS Version.'
+          echo
+          exit 1
+      esac
       echo
       echo '[*] Installing Packages'
       yum install openvpn easy-rsa
@@ -78,21 +114,17 @@ func_install(){
 
 # Server Buildout Function
 func_build_server(){
-  # Get User Input
-  func_os
-  read -p 'Enter Operating System.....................: ' os
-  # Retry For People Who Don't Read Well
-  if [[ ! ${os} =~ [1-3] ]]; then
-    func_title
-    echo
-    func_build_server
-  fi
+  # Get Server Configuration User Input
   read -p 'Enter Server Hostname......................: ' host
   echo
   echo '+----------------------+'
   echo '| Available Interfaces |'
   echo '+----------------------+'
-  ifconfig |awk "/Link|inet/"|tr -s '[:space:]'|sed 's/ Link.*//g'|sed -e ':a;N;$!ba;s/\n inet//g' -e 's/addr://g'|cut -d" " -f 1,2|sed 's/ /\t/g'
+  if [ -f /etc/redhat-release ]; then
+    ifconfig|awk "/flags|inet/"|tr -s '[:space:]'|sed -e 's/flags.*//g'|sed -e ':a;N;$!ba;s/\n inet//g'|cut -d" " -f1,3|sed -e 's/ /\t/g'
+  else
+    ifconfig|awk "/Link|inet/"|tr -s '[:space:]'|sed 's/ Link.*//g'|sed -e ':a;N;$!ba;s/\n inet//g' -e 's/addr://g'|cut -d" " -f 1,2|sed 's/ /\t/g'
+  fi
   echo
   read -p 'Enter IP OpenVPN Server Will Bind To.......: ' ip
   read -p 'Enter Subnet For VPN (ex: 192.168.100.0)...: ' vpnnet
@@ -103,11 +135,6 @@ func_build_server(){
   read -p 'Route All Traffic Through This VPN (y/n)...: ' routeall
   read -p 'Allow Certificates With Same Subject (y/n).: ' unique
   read -p 'Enable IP Forwarding (y/n).................: ' forward
-  # Determine What IP Protocols To Forward For
-  if [[ ${forward} == [yY] ]]; then
-    read -p 'Forward IPv4 (y/n).........................: ' forward4
-    read -p 'Forward IPv6 (y/n).........................: ' forward6
-  fi
   read -p 'Enable IPTables Masquerading (y/n).........: ' enablenat
   # Determine What Interface To Use For Masquerading
   if [[ ${enablenat} == [yY] ]]; then
@@ -119,19 +146,15 @@ func_build_server(){
   echo
   echo '[*] Preparing Directories'
   # Copy Easy-RSA Sample Directory
-  if [[ ${os} =~ [1-2] ]]; then
-    cp -R /usr/share/doc/openvpn/examples/easy-rsa/2.0 ${easyrsa_dir}
-  else
-    cp -R /usr/share/easy-rsa/2.0 ${easyrsa_dir}
-  fi
+  cp -R ${easyrsa_fnd} ${easyrsa_dir}
   cd ${easyrsa_dir}
   # Modify OpenSSL Variables For 2048 Bit Encryption
   if [[ ${incbits} == [yY] ]]; then
     sed -i 's/KEY_SIZE=1024/KEY_SIZE=2048/g' vars
   fi
-  # Workaround For Ubuntu 12.x
-  if [ ${os} == '2' ]; then
-    echo '[*] Preparing Ubuntu Config File'
+  # Create OpenSSL Configuration if Non-Existant
+  if [ ! -f openssl.cnf ]; then
+    echo '[*] Preparing OpenSSL Configuration'
     cp openssl-1.0.0.cnf openssl.cnf
   fi
   echo '[*] Resetting Variables'
@@ -186,10 +209,9 @@ func_build_server(){
   echo "max-clients ${maxconn}" >> ${ovpnsvr_cnf}
   echo 'user nobody' >> ${ovpnsvr_cnf}
   # Determine Unprivileged Group To Use
-  if [ ${os} == '3' ]; then
+  grp_check=$(grep -i "nogroup" /etc/group|wc -l)
+  if [ ${grp_check} != '1' ]; then
     echo 'group nobody' >> ${ovpnsvr_cnf}
-  else
-    echo 'group nogroup' >> ${ovpnsvr_cnf}
   fi
   echo 'persist-key' >> ${ovpnsvr_cnf}
   echo 'persist-tun' >> ${ovpnsvr_cnf}
@@ -197,22 +219,11 @@ func_build_server(){
   echo "log ${openvpn_dir}/openvpn.log" >> ${ovpnsvr_cnf}
   echo 'verb 3' >> ${ovpnsvr_cnf}
   echo 'mute 20' >> ${ovpnsvr_cnf}
-  # Determine If IPv4 Forwarding Option Was Chosen
-  if [[ ${forward4} == [yY] ]]; then
-    echo '[*] Enabling IPv4 Forwarding In /etc/sysctl.conf'
-    sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
-    sed -i 's/net.ipv4.ip_forward=0/net.ipv4.ip_forward=1/' /etc/sysctl.conf
-    echo '[*] Reloading sysctl Configuration'
-    /sbin/sysctl -p >> /dev/null 2>&1
-  fi
-  # Determine If IPv6 Forwarding Option Was Chosen
-  if [[ ${forward6} == [yY] ]]; then
-    echo '[*] Enabling IPv6 Forwarding In /etc/sysctl.conf'
-    sed -i 's/#net.ipv6.conf.all.forwarding=1/net.ipv6.conf.all.forwarding=1/' /etc/sysctl.conf
-    sed -i 's/net.ipv6.conf.all.forwarding=0/net.ipv6.conf.all.forwarding=1/' /etc/sysctl.conf
-    echo '[*] Reloading sysctl Configuration'
-    /sbin/sysctl -p >> /dev/null 2>&1
-  fi
+  echo '[*] Enabling IPv4 Forwarding In /etc/sysctl.conf'
+  sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
+  sed -i 's/net.ipv4.ip_forward=0/net.ipv4.ip_forward=1/' /etc/sysctl.conf
+  echo '[*] Reloading sysctl Configuration'
+  /sbin/sysctl -p >> /dev/null 2>&1
   # Determine If IPTables NAT Option Was Chosen
   if [[ ${enablenat} == [yY] ]]; then
     echo '[*] Loading IPTables Masquerading Rule Into Current Ruleset'
@@ -235,14 +246,13 @@ func_build_client(){
   echo '+------------------------+'
   echo '| Available IP Addresses |'
   echo '+------------------------+'
-  ifconfig |awk "/Link|inet/"|tr -s '[:space:]'|sed 's/ Link.*//g'|sed -e ':a;N;$!ba;s/\n inet//g' -e 's/addr://g'|cut -d" " -f 1,2|sed 's/ /\t/g'
+  if [ -f /etc/redhat-release ]; then
+    ifconfig|awk "/flags|inet/"|tr -s '[:space:]'|sed -e 's/flags.*//g'|sed -e ':a;N;$!ba;s/\n inet//g'|cut -d" " -f1,3|sed -e 's/ /\t/g'
+  else
+    ifconfig|awk "/Link|inet/"|tr -s '[:space:]'|sed 's/ Link.*//g'|sed -e ':a;N;$!ba;s/\n inet//g' -e 's/addr://g'|cut -d" " -f 1,2|sed 's/ /\t/g'
+  fi
   echo
   read -p 'Enter IP/Hostname OpenVPN Server Binds To......: ' ip
-  read -p 'Will This Client Run Under Windows (y/n).......: ' windows
-  # Additional Configuration For Windows Clients
-  if [[ ${windows} == [yY] ]]; then
-    read -p 'Enter Node Name (Required For Windows Clients).: ' node
-  fi
 
   # Build Certificate
   func_title
@@ -261,10 +271,6 @@ func_build_client(){
   echo '[*] Creating Client Configuration'
   echo 'client' > ${user}/${confname}.ovpn
   echo 'dev tun' >> ${user}/${confname}.ovpn
-  # Determine If Windows Options Were Chosen
-  if [[ ${windows} == [yY] ]]; then
-    echo "dev-node ${node}" >> ${user}/${confname}.ovpn
-  fi
   echo 'proto udp' >> ${user}/${confname}.ovpn
   echo "remote ${ip} 1194" >> ${user}/${confname}.ovpn
   echo 'resolv-retry infinite' >> ${user}/${confname}.ovpn
@@ -337,3 +343,4 @@ case ${1} in
     echo '            -c = Build Client Configuration'
     echo
 esac
+
