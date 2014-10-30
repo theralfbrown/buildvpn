f599bd3e8f76a5809bb7e80ac749afe14f76ce80
themightyshiv <shiv@areax.io>
2014-10-30 10:11:35 -0400

    Released.: Version 1.6.1 Fixed....: easy-rsa Auto-Detection Fixed....: Issue Creating VPN on Ubuntu 14 Fixed....: Cleaned Up Interfaces View Modified.: Encryption Key Size is Now User Definable Modified.: Cosmetics

--- BuildVPN.sh
+++ BuildVPN.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Global Variables
-easyrsa_fnd=$(find / -wholename "*/easy-rsa/2.0")
+easyrsa_fnd=$(find / -wholename "*/easy-rsa/*vars"|sed 's:/vars::')
 openvpn_dir='/etc/openvpn'
 easyrsa_dir='/etc/openvpn/easy-rsa'
 ovpnkey_dir='/etc/openvpn/easy-rsa/keys'
@@ -14,7 +14,7 @@ func_title(){
 
   # Print Title
   echo '================================================================'
-  echo ' BuildVPN.sh | [Version]: 1.6.0 | [Updated]: 07.13.2014'
+  echo ' BuildVPN.sh | [Version]: 1.6.1 | [Updated]: 10.30.2014'
   echo '================================================================'
 }
 
@@ -25,7 +25,8 @@ func_os(){
   echo '[ Supported Operating Systems ]'
   echo
   echo ' 1 = Debian-Based.(Versions 5+)'
-  echo ' 2 = RHEL-Based...(Versions 5+)'
+  echo ' 2 = Ubuntu ......(Versions 12-14)'
+  echo ' 3 = RHEL-Based...(Versions 5-6)'
   echo
 }
 
@@ -38,6 +39,7 @@ func_install(){
   else
     host_os=$(cat /etc/issue.net|cut -d" " -f1)
   fi
+
   # Determine OS
   if [[ ${host_os} == 'Debian' ]] || [[ ${host_os} == 'Ubuntu' ]]; then
     os='1'
@@ -50,8 +52,9 @@ func_install(){
     func_os
     read -p 'Enter Operating System.....................: ' os
   fi
+
   # OpenVPN Installer Statement
-  if [ ${os} == '1' ]; then
+  if [ ${os} == '1' ] || [ ${os} == '2' ]; then
     # Install Using Apt-Get (Debian-Based)
     func_title
     echo
@@ -60,8 +63,8 @@ func_install(){
     apt-get update
     echo
     echo '[*] Installing Packages'
-    apt-get -y install openvpn openssl
-  elif [ ${os} == '2' ]; then
+    apt-get -y install openvpn openssl easy-rsa
+  elif [ ${os} == '3' ]; then
     # Install Using Yum (RHEL-Based)
     func_title
     echo
@@ -123,7 +126,7 @@ func_build_server(){
   if [ -f /etc/redhat-release ]; then
     ifconfig|awk "/flags|inet/"|tr -s '[:space:]'|sed -e 's/flags.*//g'|sed -e ':a;N;$!ba;s/\n inet//g'|cut -d" " -f1,3|sed -e 's/ /\t/g'
   else
-    ifconfig|awk "/Link|inet/"|tr -s '[:space:]'|sed 's/ Link.*//g'|sed -e ':a;N;$!ba;s/\n inet//g' -e 's/addr://g'|cut -d" " -f 1,2|sed 's/ /\t/g'
+    ifconfig|awk "/Link |inet /"|tr -s '[:space:]'|sed 's/ Link.*//g'|sed -e ':a;N;$!ba;s/\n inet//g' -e 's/addr://g'|cut -d" " -f 1,2|sed 's/ /\t/g'|awk '!/lo/'
   fi
   echo
   read -p 'Enter IP OpenVPN Server Will Bind To.......: ' ip
@@ -131,12 +134,19 @@ func_build_server(){
   read -p 'Enter Subnet Netmask (ex: 255.255.255.0)...: ' netmsk
   read -p 'Enter Preferred DNS Server (ex: 8.8.8.8)...: ' dns
   read -p 'Enter Max Clients Threshold................: ' maxconn
-  read -p 'Increase Encryption Key Size To 2048 (y/n).: ' incbits
+  read -p 'Increase Encryption Key Size (y/n).........: ' incbits
+
+# Get User Increased Key Size
+  if [[ ${incbits} == [yY] ]]; then
+    read -p 'Enter Encryption Key Size (ex: 2048).......: ' keysize
+  fi
+
   read -p 'Route All Traffic Through This VPN (y/n)...: ' routeall
   read -p 'Allow Certificates With Same Subject (y/n).: ' unique
   read -p 'Enable IP Forwarding (y/n).................: ' forward
   read -p 'Enable IPTables Masquerading (y/n).........: ' enablenat
-  # Determine What Interface To Use For Masquerading
+
+# Determine What Interface To Use For Masquerading
   if [[ ${enablenat} == [yY] ]]; then
     read -p 'Enter Interface To Use For Masquerading....: ' natif
   fi
@@ -145,18 +155,22 @@ func_build_server(){
   func_title
   echo
   echo '[*] Preparing Directories'
+
   # Copy Easy-RSA Sample Directory
   cp -R ${easyrsa_fnd} ${easyrsa_dir}
   cd ${easyrsa_dir}
-  # Modify OpenSSL Variables For 2048 Bit Encryption
+
+  # Update vars With User Specified Key Size
   if [[ ${incbits} == [yY] ]]; then
-    sed -i 's/KEY_SIZE=1024/KEY_SIZE=2048/g' vars
+    sed -i "s/KEY_SIZE=.*$/KEY_SIZE=${keysize}/g" vars
   fi
+
   # Create OpenSSL Configuration if Non-Existant
   if [ ! -f openssl.cnf ]; then
     echo '[*] Preparing OpenSSL Configuration'
     cp openssl-1.0.0.cnf openssl.cnf
   fi
+
   echo '[*] Resetting Variables'
   . ./vars >> /dev/null
   echo '[*] Preparing Build Configurations'
@@ -186,33 +200,37 @@ func_build_server(){
   echo "ca ${ovpnkey_dir}/ca.crt" >> ${ovpnsvr_cnf}
   echo "cert ${ovpnkey_dir}/${host}.crt" >> ${ovpnsvr_cnf}
   echo "key ${ovpnkey_dir}/${host}.key" >> ${ovpnsvr_cnf}
-  # Determine If Increased Key Size Option Was Chosen
-  if [[ ${incbits} == [yY] ]]; then
-    echo "dh ${ovpnkey_dir}/dh2048.pem" >> ${ovpnsvr_cnf}
-  else
-    echo "dh ${ovpnkey_dir}/dh1024.pem" >> ${ovpnsvr_cnf}
-  fi
+
+  # Get DH Key
+  dhkey=$(find /etc/openvpn/easy-rsa -name "*dh*.pem")
+  echo "dh ${dhkey}" >> ${ovpnsvr_cnf}
+
   echo "server ${vpnnet} ${netmsk}" >> ${ovpnsvr_cnf}
   echo 'ifconfig-pool-persist ipp.txt' >> ${ovpnsvr_cnf}
+
   # Determine If Route All Traffic Option Was Chosen
   if [[ ${routeall} == [yY] ]]; then
     echo 'push "redirect-gateway def1"' >> ${ovpnsvr_cnf}
   fi
+
   # Determine If Unique Subjects Option Was Chosen
   if [[ ${unique} == [yY] ]]; then
     echo 'unique_subject = no' >> ${openvpn_dir}/easy-rsa/keys/index.txt.attr
   fi
+
   echo "push "dhcp-option DNS ${dns}"" >> ${ovpnsvr_cnf}
   echo 'keepalive 10 120' >> ${ovpnsvr_cnf}
   echo "tls-auth ${ovpnkey_dir}/ta.key 0" >> ${ovpnsvr_cnf}
   echo 'comp-lzo' >> ${ovpnsvr_cnf}
   echo "max-clients ${maxconn}" >> ${ovpnsvr_cnf}
   echo 'user nobody' >> ${ovpnsvr_cnf}
+
   # Determine Unprivileged Group To Use
   grp_check=$(grep -i "nogroup" /etc/group|wc -l)
   if [ ${grp_check} != '1' ]; then
     echo 'group nobody' >> ${ovpnsvr_cnf}
   fi
+
   echo 'persist-key' >> ${ovpnsvr_cnf}
   echo 'persist-tun' >> ${ovpnsvr_cnf}
   echo "status ${openvpn_dir}/status.log" >> ${ovpnsvr_cnf}
@@ -224,6 +242,7 @@ func_build_server(){
   sed -i 's/net.ipv4.ip_forward=0/net.ipv4.ip_forward=1/' /etc/sysctl.conf
   echo '[*] Reloading sysctl Configuration'
   /sbin/sysctl -p >> /dev/null 2>&1
+
   # Determine If IPTables NAT Option Was Chosen
   if [[ ${enablenat} == [yY] ]]; then
     echo '[*] Loading IPTables Masquerading Rule Into Current Ruleset'
@@ -232,6 +251,7 @@ func_build_server(){
     sed -i "s:exit 0:/sbin/iptables -t nat -A POSTROUTING -o ${natif} -j MASQUERADE:" /etc/rc.local
     echo "exit 0" >> /etc/rc.local
   fi
+
   # Finish Message
   echo '[*] Server Buildout Complete'
   echo
@@ -249,7 +269,7 @@ func_build_client(){
   if [ -f /etc/redhat-release ]; then
     ifconfig|awk "/flags|inet/"|tr -s '[:space:]'|sed -e 's/flags.*//g'|sed -e ':a;N;$!ba;s/\n inet//g'|cut -d" " -f1,3|sed -e 's/ /\t/g'
   else
-    ifconfig|awk "/Link|inet/"|tr -s '[:space:]'|sed 's/ Link.*//g'|sed -e ':a;N;$!ba;s/\n inet//g' -e 's/addr://g'|cut -d" " -f 1,2|sed 's/ /\t/g'
+    ifconfig|awk "/Link |inet /"|tr -s '[:space:]'|sed 's/ Link.*//g'|sed -e ':a;N;$!ba;s/\n inet//g' -e 's/addr://g'|cut -d" " -f 1,2|sed 's/ /\t/g'|awk '!/lo/'
   fi
   echo
   read -p 'Enter IP/Hostname OpenVPN Server Binds To......: ' ip
